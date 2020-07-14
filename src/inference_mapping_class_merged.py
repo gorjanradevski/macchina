460fae93ea809dbbf4f38c67ec2c82ded06b1a6e
duka <dusangr22@gmail.com>
2020-07-15 00:15:28 +0200

    added evaluation for merged organs - classification

---
+++ src/inference_mapping_class_merged.py
@@ -0,0 +1,195 @@
+import argparse
+import json
+import os
+import random
+
+import numpy as np
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from transformers import BertConfig, BertTokenizer
+
+from voxel_mapping.datasets import (
+    VoxelSentenceMappingTestClassDataset,
+    collate_pad_sentence_class_batch,
+)
+from voxel_mapping.evaluator import InferenceEvaluatorPerOrgan
+from voxel_mapping.models import ClassModel
+
+
+def inference(
+    test_json_path: str,
+    organs_dir_path: str,
+    organs_dir_merged_path: str,
+    voxelman_images_path: str,
+    batch_size: int,
+    bert_name: str,
+    random_organ_point: bool,
+    checkpoint_path: str,
+):
+    # Check for CUDA
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # Load jsons
+    ind2organ = json.load(open(os.path.join(organs_dir_path, "ind2organ.json")))
+    organ2label = json.load(open(os.path.join(organs_dir_path, "organ2label.json")))
+    organ2voxels = json.load(open(os.path.join(organs_dir_path, "organ2voxels.json")))
+
+    ind2organ_merged = json.load(
+        open(os.path.join(organs_dir_merged_path, "ind2organ.json"))
+    )
+    organ2voxels_merged = json.load(
+        open(os.path.join(organs_dir_merged_path, "organ2voxels.json"))
+    )
+    organ2center_merged = json.load(
+        open(os.path.join(organs_dir_merged_path, "organ2center.json"))
+    )
+    # Load organ to indices to obtain the number of classes
+    num_classes = max([int(index) for index in ind2organ_merged.keys()]) + 1
+    tokenizer = BertTokenizer.from_pretrained(bert_name)
+    test_dataset = VoxelSentenceMappingTestClassDataset(
+        test_json_path, tokenizer, num_classes
+    )
+    test_loader = DataLoader(
+        test_dataset, batch_size=batch_size, collate_fn=collate_pad_sentence_class_batch
+    )
+    config = BertConfig.from_pretrained(bert_name)
+    model = nn.DataParallel(
+        ClassModel(bert_name, config, final_project_size=num_classes)
+    ).to(device)
+    model.load_state_dict(torch.load(checkpoint_path, map_location=device))
+    # Set model in evaluation mode
+    model.train(False)
+    # Create evaluator
+    evaluator = InferenceEvaluatorPerOrgan(
+        ind2organ, organ2label, organ2voxels, voxelman_images_path, len(test_dataset)
+    )
+    with torch.no_grad():
+        evaluator.reset_counters()
+        for sentences, attn_mask, organs_indices, _ in tqdm(test_loader):
+            sentences, attn_mask = sentences.to(device), attn_mask.to(device)
+            output_mappings = model(input_ids=sentences, attention_mask=attn_mask)
+            y_pred = torch.argmax(output_mappings, dim=-1)
+            if random_organ_point:
+                pred_points = [
+                    random.sample(
+                        organ2voxels_merged[ind2organ_merged[str(ind.item())]], 1
+                    )[0]
+                    for ind in y_pred
+                ]
+            else:
+                pred_points = [
+                    organ2center_merged[ind2organ_merged[str(ind.item())]]
+                    for ind in y_pred
+                ]
+            for pred_point, organ_indices in zip(pred_points, organs_indices):
+                evaluator.update_counters(
+                    np.array(pred_point), np.where(organ_indices == 1)[0]
+                )
+
+        print(
+            "The avg IOR on the test set is: "
+            f"{evaluator.get_current_ior()} +/- {evaluator.get_ior_error_bar()}"
+        )
+        print(
+            "The avg distance on the test set is: "
+            f"{evaluator.get_current_distance()} +/- {evaluator.get_distance_error_bar()}"
+        )
+        print(
+            "The avg miss distance on the test set is: "
+            f"{evaluator.get_current_miss_distance()} +/- {evaluator.get_miss_distance_error_bar()}"
+        )
+        print("============================================")
+        for organ_name in evaluator.organ_names:
+            if evaluator.get_current_ior_for_organ(organ_name) > -1:
+                print(
+                    f"The avg IOR for {organ_name} is: "
+                    f"{evaluator.get_current_ior_for_organ(organ_name)} +/- "
+                    f"{evaluator.get_ior_error_bar_for_organ(organ_name)}"
+                )
+                print(
+                    f"The avg NVD {organ_name} is: "
+                    f"{evaluator.get_current_distance_for_organ(organ_name)} +/- "
+                    f"{evaluator.get_distance_error_bar_for_organ(organ_name)}"
+                )
+                print(
+                    f"The avg NVD-O {organ_name} is: "
+                    f"{evaluator.get_current_miss_distance_for_organ(organ_name)} +/- "
+                    f"{evaluator.get_miss_distance_error_bar_for_organ(organ_name)}"
+                )
+                print("============================================")
+
+
+def main():
+    # Without the main sentinel, the code would be executed even if the script were
+    # imported as a module.
+    args = parse_args()
+    inference(
+        args.test_json_path,
+        args.organs_dir_path,
+        args.organ_dir_merged_path,
+        args.voxelman_images_path,
+        args.batch_size,
+        args.bert_name,
+        args.random_organ_point,
+        args.checkpoint_path,
+    )
+
+
+def parse_args():
+    """Parse command line arguments.
+    Returns:
+        Arguments
+    """
+    parser = argparse.ArgumentParser(
+        description="Inference with a sentence voxel mapping model."
+    )
+    parser.add_argument(
+        "--organs_dir_path",
+        type=str,
+        default="data/data_organs",
+        help="Path to the data organs directory.",
+    )
+    parser.add_argument(
+        "--organs_dir_merged_path",
+        type=str,
+        help="Path to the data organs directory with merged organs.",
+    )
+    parser.add_argument(
+        "--voxelman_images_path",
+        type=str,
+        default="data/voxelman_images",
+        help="Path to the voxelman images.",
+    )
+    parser.add_argument(
+        "--test_json_path",
+        type=str,
+        default="data/dataset_text_atlas_mapping_test_fixd.json",
+        help="Path to the test set",
+    )
+    parser.add_argument(
+        "--batch_size", type=int, default=128, help="The size of the batch."
+    )
+    parser.add_argument(
+        "--bert_name",
+        type=str,
+        default="bert-base-uncased",
+        help="The pre-trained Bert model.",
+    )
+    parser.add_argument(
+        "--random_organ_point",
+        action="store_true",
+        help="Whether to take a random point in the organ as opposed to the center",
+    )
+    parser.add_argument(
+        "--checkpoint_path",
+        type=str,
+        default=None,
+        help="Path to a pretrained checkpoint.",
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
